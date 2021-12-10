d68a1dd59211fefdff7af4302f1cb866746e2cc5
Vlad Vojvo <vlad.vojvo86@gmail.com>
2021-12-11 00:16:33 +1100

    html, css

--- pages/home.js
+++ pages/home.js
@@ -17,16 +17,22 @@ export default function Home() {
                 
             </div>        
             <div className={styles.col2}>
-            <h1 className={styles.title}>plant name</h1>
-            <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+            <h1 className={styles.title}>Arizona snowcap</h1>
+            <img className={styles.picture} src="../images/snowcap.jpg" />
+            <li>Latin name: Mammillaria Gracilis cultivar</li>
+            <li>Watering:</li>
+            <li>Positioning:</li>
+            </div> 
+            <div className={styles.col2}>
+            <h1 className={styles.title}>Christmas cactus</h1>
+            <img className={styles.picture} src="../images/zygocactus.jpg" />
+            <li>Latin name: Zygocactus Schlumbergera</li>
+            <li>Watering:</li>
+            <li>Positioning:</li>
             </div> 
             <div className={styles.col2}>
             <h1 className={styles.title}>plant name</h1>
-            <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
-            </div> 
-            <div className={styles.col2}>
-            <h1 className={styles.title}>plant name</h1>
-            <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div> 
             <div className={styles.col3}>
             
